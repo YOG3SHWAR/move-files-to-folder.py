05370126c64cd75a3bef330d54b0ea8a8c1996c3
Yogeshwar Chaturvedi <56862396+YOG3SHWAR@users.noreply.github.com>
2021-09-06 23:57:13 +0530

    Add files via upload

---
+++ move-files-to-folder.py
@@ -0,0 +1,31 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# pip install watchdog
+
+import os
+import json
+import time
+
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            src = folder_to_track + "/" + filename
+            new_destination = folder_destination + "/" + filename
+            os.rename(src, new_destination)
+
+
+folder_to_track = "/home/ogeshwary/Projects/test/1"
+folder_destination = "/home/ogeshwary/Projects/test/23"
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
